f0baa4ca8194ee8588dda78276ae38bf6dec76a3
solnikk <solodovnikov.2003@gmail.com>
2025-03-25 22:30:26 +0300

    version3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './ProductCard.css'
-import './Carousel.css'
+import './styles/variables.css'
+import './styles/Header.css'
+import './styles/Footer.css'
+import './styles/PopularProducts.css'
+import './styles/FavoriteList.css'
+import './styles/ProductCard.css'
+import './styles/ProductDetails.css'
+import './styles/Carousel.css'
+import './styles/BackButton.css'
+import './styles/ProductList.css'
 import './App.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+
+// Добавляем стили для предотвращения горизонтальной прокрутки
+const style = document.createElement('style')
+style.textContent = `
+  html, body {
+    overflow-x: hidden;
+    max-width: 100%;
+    width: 100%;
+    position: relative;
+  }
+`
+document.head.appendChild(style)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
